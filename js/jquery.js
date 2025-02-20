832c5b3cca7f6fac1fa7eaf2bd8de2e7a24c9436
zisu <wlsu00@naver.com>
2025-02-20 18:19:52 +0900

    [feat]서브 chips 클릭상태 색상변경

--- js/jquery.js
+++ js/jquery.js
@@ -7,4 +7,13 @@ $(function () {
     $(".search_bar input").slideToggle(400);
     $("#logo").toggle(10);
   });
+
+  //chips 클릭하면 클래스명 추가해서 css 바꾸기
+  let mainChips = $(".chips_con .chips");
+  //a버튼 누르면 거기에 클래스명 추가
+  //그렇지않으면 다른 버튼에 클래스명 추가
+  //실습했던거 보고 해보기
+  $(".chips_con .chips").on("click", function () {
+    $(".chips_con .chips").addClass("main_chips");
+  });
 });
